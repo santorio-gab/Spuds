4f6b1e41efa794e3784c6cd164fd0f749aaa0954
mant45 <mantaspostbox@gmail.com>
2022-08-17 12:56:56 +0100

    add if statement to filter 'out of stock' items

--- potato_soup.py
+++ potato_soup.py
@@ -8,13 +8,15 @@ soup = BeautifulSoup(result.text, "html.parser")
 
 prod_list = soup.find_all('div', {"class":"styles__StyledVerticalTileWrapper-dvv1wj-0 dtCNPH"})
 spuds = {}
-
+ 
 for product in prod_list:
-    name = product.find('span', {"class":"styled__Text-sc-1xbujuz-1 ldbwMG beans-link__text"}).text
-    price_kg = product.find('p', {"class":"styled__StyledFootnote-sc-119w3hf-7 icrlVF styled__Subtext-sc-8qlq5b-2 bNJmdc beans-price__subtext"}).text[1:-3]
-    if '/' not in price_kg:
-        price_kg = float(price_kg)
-        spuds.update({f'{name}' : f'{price_kg}'})
+    stock_check = product.find('div', {"class":"product-info-message-list"}).text
+    if 'out of' not in  stock_check:
+        name = product.find('span', {"class":"styled__Text-sc-1xbujuz-1 ldbwMG beans-link__text"}).text
+        price_kg = product.find('p', {"class":"styled__StyledFootnote-sc-119w3hf-7 icrlVF styled__Subtext-sc-8qlq5b-2 bNJmdc beans-price__subtext"}).text[1:-3]
+        if '/' not in price_kg:
+            price_kg = float(price_kg)
+            spuds.update({f'{name}' : f'{price_kg}'})
 
 spuds_sorted = dict(sorted(spuds.items(), key= lambda x:x[1]))
 
